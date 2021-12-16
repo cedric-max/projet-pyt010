26f3d08d1e27bc1ed704f87caca1da73e1aec106
Thibault Bruggeman <bruggeman.dev@gmail.com>
2021-12-16 16:56:55 +0100

    Add feature delete & add todo

--- app.py
+++ app.py
@@ -5,15 +5,16 @@ import sqlite3
 ### CONSTANTS ###
 app = Flask(__name__)
 SQL_SCRIPT = "./app.sql"
-app.config['DATABASE'] = "./app.db"
-app.config['SECRET_KEY'] = '57d907f1370fb2b6e5ca21ef55e584ef8240dcdecbfcc35e8389cdd2751e52bb'
+app.config["DATABASE"] = "./app.db"
+app.config[
+    "SECRET_KEY"
+] = "57d907f1370fb2b6e5ca21ef55e584ef8240dcdecbfcc35e8389cdd2751e52bb"
 
-### DATABASE OPERATIONS ###
+### DATABASE ###
 
 # Initialize database
 with open(SQL_SCRIPT) as fd:
-    print("Database connection...")
-    connection = sqlite3.connect(app.config['DATABASE'])
+    connection = sqlite3.connect(app.config["DATABASE"])
     cursor = connection.cursor()
     cursor.executescript(fd.read())
     print("Database initialized with success!")
@@ -21,102 +22,96 @@ with open(SQL_SCRIPT) as fd:
 
 # Connect and get database
 def get_db():
-    if 'db' not in g:
+    if "db" not in g:
         g.db = sqlite3.connect(
-            app.config['DATABASE'],
-            detect_types=sqlite3.PARSE_DECLTYPES
+            app.config["DATABASE"], detect_types=sqlite3.PARSE_DECLTYPES
         )
         g.db.row_factory = sqlite3.Row
 
     return g.db
 
-### ROUTES ###
-@app.route("/")
-def index():
-    user_id = session.get('user_id')
-    if user_id is None:
-        return redirect(url_for('register'))
-    else:
-        todos = get_db().execute(
-            "SELECT oid, ToDoName FROM todos WHERE UserId = ?", (user_id,)
-        ).fetchall()
-        todo_list = []
-        for todo in todos :
-            row = {"id": todo['rowid'], "name": todo['ToDoName']}
-            todo_list.append(row)
-    return render_template('index.html', todo_list=todo_list)
 
-@app.route("/register", methods=('GET', 'POST'))
+### ROUTES ###
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
         db = get_db()
         error = None
         user = db.execute(
-            'SELECT oid, UserName, UserPassword FROM users WHERE UserName = ?;', (username,)
+            "SELECT oid, UserName, UserPassword FROM users WHERE UserName = ?",
+            (username,),
         ).fetchone()
 
         if user is None:
-            error = 'Incorrect username.'
-        elif not check_password_hash(user['UserPassword'], password):
-            error = 'Incorrect password.'
+            error = "Incorrect username."
+        elif not check_password_hash(user["UserPassword"], password):
+            error = "Incorrect password."
 
         if error is None:
             session.clear()
-            session['user_id'] = user['rowid']
-            return redirect(url_for('index'))
+            session["user_id"] = user["rowid"]
+            return redirect(url_for("index"))
 
         flash(error)
 
-    return render_template('register.html')
+    return render_template("register.html")
 
-@app.route('/logout')
+@app.route("/logout")
 def logout():
     session.clear()
-    return redirect(url_for('register'))
+    return redirect(url_for("register"))
 
-#Method
+@app.route("/")
+def index():
+    user_id = session.get("user_id")
+    if user_id is None:
+        return redirect(url_for("register"))
+    else:
+        todos = (
+            get_db()
+            .execute("SELECT oid, ToDoName FROM todos WHERE UserId = ?", (user_id,))
+            .fetchall()
+        )
+        todo_list = []
+        for todo in todos:
+            row = {"id": todo["rowid"], "name": todo["ToDoName"]}
+            todo_list.append(row)
+    return render_template("index.html", todo_list=todo_list, user_id=user_id)
 
-#Implementation Add todos
-@app.route("/edit/<id>", methods=('GET','POST'))
+@app.route("/add", methods=["POST"])
+def add():
+    todo_name = request.form["name"]
+    user_id = session.get("user_id")
+    db = get_db()
+    db.execute("INSERT INTO todos VALUES (?,?)", (todo_name, user_id))
+    db.commit()
+    return redirect(url_for("index"))
+
+@app.route("/edit/<id>", methods=("GET", "POST"))
 def edit(id):
-    if request.method == 'POST':
+    if request.method == "POST":
         name = request.form["name"]
         db = get_db()
-        db.execute(
-            'UPDATE todos SET ToDoName = ? WHERE oid = ?;', (name, id)
-        )
+        db.execute("UPDATE todos SET ToDoName = ? WHERE oid = ?", (name, id))
         db.commit()
-        return redirect(url_for('index'))
+        return redirect(url_for("index"))
 
     db = get_db()
-    row = db.execute(
-        'SELECT rowid, ToDoName FROM todos WHERE oid = ?;', (id,)
+    todo_row = db.execute(
+        "SELECT rowid, ToDoName FROM todos WHERE oid = ?", (id,)
     ).fetchone()
-    todo = {"id": row['rowid'], "name": row['ToDoName']}
-    return render_template('edit.html', todo=todo)
+    todo = {"id": todo_row["rowid"], "name": todo_row["ToDoName"]}
+    return render_template("edit.html", todo=todo)
 
-
-#Implementation Delete todos
-@app.route("/delete/<id>", methods=['POST'])
+@app.route("/delete/<id>", methods=["POST"])
 def delete(id):
-    pass
-#     if request.method == 'POST':
-#         db = get_db()
-#         error = None
-#         user = db.execute(
-#             "ALTER TABLE todos DROP COLUMN ToDoName;"
-#         ).fetchone()
-#     return redirect(url_for('index.html'))
-
-
-# @app.route("/test")
-# def test():
-#     todo_list = [{"name" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam id rutrum turpis.","id" : 1},
-#     {"name" : "Sed malesuada ipsum diam, sed elementum tellus porttitor ut. Aenean ornare turpis eget ligula iaculis, in ullamcorper dolor ultrices","id" : 2},
-#     {"name" : "Morbi convallis non mauris ut auctor. Integer ac laoreet purus, sit amet lacinia urna. Curabitur nec bibendum libero.","id" : 3}]
-#     return render_template('index.html', todo_list=todo_list)
+    user_id = session.get("user_id")
+    db = get_db()
+    db.execute("DELETE FROM todos WHERE oid = ? AND UserId = ?", (id, user_id))
+    db.commit()
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
     app.run(debug=True)
--- app.sql
+++ app.sql
@@ -18,15 +18,17 @@ CREATE TABLE IF NOT EXISTS users
 );
 CREATE INDEX [IDX_UserName] ON users([UserName]);
 
-INSERT INTO users VALUES ('Tibow', 'pbkdf2:sha256:260000$RKKOdIKFmtoRa4Xt$2dfcdb963ce6b6ab1db2fe80c1765f87e9172f686efff2ac373c7785e013346f'),
-('Cédric', 'pbkdf2:sha256:260000$RKKOdIKFmtoRa4Xt$2dfcdb963ce6b6ab1db2fe80c1765f87e9172f686efff2ac373c7785e013346f'),
-('Thibaut', 'pbkdf2:sha256:260000$RKKOdIKFmtoRa4Xt$2dfcdb963ce6b6ab1db2fe80c1765f87e9172f686efff2ac373c7785e013346f');
+INSERT INTO users VALUES 
+    ('Joe', 'pbkdf2:sha256:260000$RKKOdIKFmtoRa4Xt$2dfcdb963ce6b6ab1db2fe80c1765f87e9172f686efff2ac373c7785e013346f'),
+    ('William', 'pbkdf2:sha256:260000$RKKOdIKFmtoRa4Xt$2dfcdb963ce6b6ab1db2fe80c1765f87e9172f686efff2ac373c7785e013346f'),
+    ('Doug', 'pbkdf2:sha256:260000$RKKOdIKFmtoRa4Xt$2dfcdb963ce6b6ab1db2fe80c1765f87e9172f686efff2ac373c7785e013346f');
 
-INSERT INTO todos(ToDoName, UserId) VALUES("Faire la vaisselle",1),
-("Appeler maman",2),
-("Libérer le Kraken",3),
-("Faire la vaisselle",1),
-("Fendre du bois",2),
-("Prendre Fany",3),
-("Faire le ménage",4),
-("Retrouver Dédé au PMU",1);
+INSERT INTO todos VALUES
+    ("Faire la ménage",1),
+    ("Appeler maman",2),
+    ("Libérer le Kraken",3),
+    ("Faire la vaisselle",1),
+    ("Fendre du bois",2),
+    ("Acheter du lait",3),
+    ("Faire le ménage",2),
+    ("Retrouver Dédé au PMU",1);
